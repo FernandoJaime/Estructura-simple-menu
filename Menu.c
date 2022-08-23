583b211aacf7ccbb4d3f4af30d34a5fc2c68a92f
Fer Jaime <Fernando.Jaime@alumnos.uai.edu.ar>
2022-08-23 08:51:09 -0300

    Seguna version de menu agregado opcion 3

--- Menu.c
+++ Menu.c
@@ -6,15 +6,18 @@ int main () {
     while(op != 0) {
         printf("1- opcion 1 \n");
         printf("2- opcion 2 \n");
+        printf("3- opcion 3 \n");
         printf("0- salir \n");
         do { //Pedir que ingrese la opcion, como es un do-while es lo primero que muestra antes de verificar
             printf("Ingrese la opcion: \n");
             scanf("%d", &op);
-        }while (op < 0 || op > 2); //Esto tambien se puede hacer con una negacion: (! (op >= 0 && op <= 2))
+        }while (op < 0 || op > 3); //Esto tambien se puede hacer con una negacion: (! (op >= 0 && op <= 2))
         switch (op) {
             case 1: printf("Estoy en la opcion 1 \n"); 
             break;
             case 2: printf("Estoy en la opcion 2 \n");
+            break;
+            case 3: printf("Estoy en la opcion 3 \n");
             break;
             case 0: printf("Saliendo del menu \n");
             break; 
